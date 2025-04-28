fb42cb565fbabb83728f08426616c73cd6cfcf62
solomoni161514 <solomonibujiashvili0@gmail.com>
2025-04-28 15:43:58 +0400

    GOA BEST!

--- level62/clsswork/test.py
+++
@@ -1,6 +0,0 @@
-def grow(arr):
-    res = 1
-    for i in arr:
-        res *= i
-    return res
-print(grow([1, 2, 3, 4]))
--- level70/classwork/script.js
+++
@@ -1,9 +0,0 @@
-let saxeli = "სოლომონი";
-let gvari = "ბუჯიაშვილი";
-let asaki = 12;
-let sayvareliFeri = "მწვანე";
-
-let x = "სახელი: " + saxeli + ", გვარი: " + gvari + ", ასაკი: " + asaki + ", ფერი: " + sayvareliFeri;
-
-console.log(x);
-alert(x);
